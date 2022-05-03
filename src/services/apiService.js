5bb511369f2b4c02c010b3b4ec776bee38d11bd6
Carlos Bausá Carmona <litos@MacBook-Pro-de-Carlos.local>
2022-05-03 20:42:47 +0200

    modify headers apiservice

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -7,6 +7,7 @@ class ApiService {
       headers: {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+        "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token",
         "Content-Type": "application/json",
       },
       withCredentials: false,
